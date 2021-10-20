2b4f05f2e4f010fd70640e7a1b44297f961334e6
Adam <asoliyev13@gmail.com>
2021-10-20 07:30:04 -0400

    Added merge sort

---
+++ mergeSort.cpp
@@ -0,0 +1,59 @@
+#include "atl.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+void merge(vector<int> &nums, int left, int mid, int right) {
+  // find sizes
+  auto left_half_size = mid - left + 1;
+  auto right_half_size = right - mid;
+
+  // create arr arrays
+  vector<int> left_arr(left_half_size);
+  vector<int> right_arr(right_half_size);
+
+  // copy data from nums to left & right arr arrays
+  for (auto i = 0; i < left_half_size; ++i) {
+    left_arr[i] = nums[left + i];
+  }
+  for (auto i = 0; i < right_half_size; ++i) {
+    right_arr[i] = nums[mid + 1 + i];
+  }
+
+  auto idx_left_arr = 0, idx_right_arr = 0;
+  auto idx_merged_arr = left;
+  // merge two arr array back to nums
+  while (idx_left_arr < left_half_size && idx_right_arr < right_half_size) {
+    if (left_arr[idx_left_arr] <= right_arr[idx_right_arr]) {
+      nums[idx_merged_arr++] = left_arr[idx_left_arr++];
+    } else {
+      nums[idx_merged_arr++] = right_arr[idx_right_arr++];
+    }
+  }
+
+  // copy remaining nums of
+  // left array, if there are any
+  while (idx_left_arr < left_half_size) {
+    nums[idx_merged_arr++] = left_arr[idx_left_arr++];
+  }
+  // right array, if there are any
+  while (idx_right_arr < right_half_size) {
+    nums[idx_merged_arr++] = right_arr[idx_right_arr++];
+  }
+}
+
+void mergeSort(vector<int> &nums, int left, int right) {
+  if (left >= right)
+    return;
+  auto mid = left + (right - left) / 2;
+  mergeSort(nums, left, mid);
+  mergeSort(nums, mid + 1, right);
+  merge(nums, left, mid, right);
+}
+
+int main() {
+  vector<int> nums{3, 5, 1, 6, 3, 0, -3, 9, -25, 34};
+  mergeSort(nums, 0, nums.size() - 1);
+  atl::printV(nums);
+
+  return 0;
+}
